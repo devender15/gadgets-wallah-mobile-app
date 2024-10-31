129f2e86ab3df0d7a91b6d29f56b6761a03423ba
Devender Pal <57867128+devender15@users.noreply.github.com>
2024-10-31 16:16:10 +0530

    Refactor tab bar styling in _layout.tsx

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,20 +1,59 @@
 import { Tabs } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) {
+  return <FontAwesome size={24} style={{ color: '#1BC464' }} {...props} />;
+}
 
 const TabsLayout = () => {
   return (
-    <SafeAreaView>
-      <Tabs>
+    <SafeAreaView edges={['top']} style={styles.safeArea}>
+      <Tabs screenOptions={
+        {
+          tabBarActiveTintColor: "#1BC464",
+          tabBarInactiveTintColor: "gray",
+          tabBarLabelStyle: {
+            fontSize: 16,
+          },
+          tabBarStyle: {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingTop: 10,
+          },
+          headerShown: false,
+        }
+      }>
         <Tabs.Screen
           name="index"
           options={{
-            headerShown: false,
+            title: 'Shop',
+            tabBarIcon(props) {
+              return <TabBarIcon name="shopping-cart" {...props} />;
+            }
           }}
         />
-        <Tabs.Screen name="orders" options={{}} />
+        <Tabs.Screen name="orders" 
+        options={{
+          title: 'Orders',
+          tabBarIcon(props) {
+            return <TabBarIcon name="book" {...props} />;
+          }
+        }} />
       </Tabs>
     </SafeAreaView>
   );
 };
 
 export default TabsLayout;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    // backgroundColor: "#fff",
+  },
+});
