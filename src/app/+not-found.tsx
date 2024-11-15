a0305252c72029a0abe3ce4affffb816acc96013
Devender Pal <57867128+devender15@users.noreply.github.com>
2024-11-15 18:56:32 +0530

    Add NotFoundScreen component for handling non-existent routes

---
+++ src/app/+not-found.tsx
@@ -0,0 +1,21 @@
+import { Link, Stack } from "expo-router";
+import { View, StyleSheet } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops! this screen doesnt exists." }} />
+      <View style={styles.container}>
+        <Link href="/">Go to home screen</Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
